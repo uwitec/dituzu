99b6bed93f433eb75d4ce46d926406b194324ddd
huster.zhangpeng@gmail.com <huster.zhangpeng@gmail.com@82cfd767-c828-be15-7ea6-d8ecbedf2070>
2013-04-10 03:42:25 +0000

    publish页面完成自动定位自动填充省市区信息

--- trunk/js/publish.js
+++ trunk/js/publish.js
@@ -15,18 +15,45 @@ function initialize() {
 		poinum: 2
 	};
 	geo = new AMap.Geocoder(GeocoderOption);
-	
 	mapObj.plugin(["AMap.ToolBar"], function(){
 		tool = new AMap.ToolBar({
 			direction:true,
 			ruler:true,
-			autoPosition:true
+			autoPosition:false
 		});
+		
 		mapObj.addControl(tool);
-	});
-	var centerPoint = mapObj.getCenter();
-	geo.regeocode(centerPoint, function(data){
-		alert(data.info);
+		tool.doLocation();
+		var counter = 0;
+		function getLocationAfterAutoPosition(){
+			var obj = tool.getLocation();
+			if(counter > 5)
+				return;
+			if(!obj){
+				counter ++;
+				setTimeout(getLocationAfterAutoPosition, 1000);
+			}else{
+				document.getElementById("map_x").value = obj.center.lng; 
+				document.getElementById("map_y").value = obj.center.lat; 
+				geo.regeocode(obj.center, function(data){
+					if(data.status == "E0"){
+						var province, city, district;
+						province = data.list[0].province.name;
+						if(province == "北京市" || province == "上海市" 
+							|| province == "天津市" || province == "重庆市"){
+							city = province;
+						} else {
+							city = data.list[0].city.name;
+						}
+						district = data.list[0].district.name;
+						document.getElementById("_province").value = province;
+						document.getElementById("_city").value = city;
+						document.getElementById("_area").value = district;
+					}
+				});
+			}
+		}
+		getLocationAfterAutoPosition();
 	});
 
 	mapObj.bind(mapObj,"click",function(e){
