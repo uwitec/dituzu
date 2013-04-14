dd8939251afc33814b73ca0c93077fbf33b8b31e
huster.zhangpeng <huster.zhangpeng@82cfd767-c828-be15-7ea6-d8ecbedf2070>
2013-04-14 07:33:46 +0000

    publish.php success to insert into brief_info

--- php/publish.php
+++ php/publish.php
@@ -1,5 +1,18 @@
 <?PHP
-    $lat = $_REQUEST["lat"];
-    $lng = $_REQUEST["lng"];
-    
+$lat = 11.345; // $_REQUEST["lat"];
+$lng = 22.345; //$_REQUEST["lng"];
+$imgSrc = "test"; //$_REQUEST["imgSrc"];
+$area = 50; //$_REQUEST["area"];
+$price = 5000; //$_REQUEST["price"];
+$rooms = 2; //$_REQUEST["rooms"];
+$halls = 3; //$_REQUSET["halls"];
+$level = 1;
+
+$con = mysql_connect("localhost", "root", "111111")
+    or die("Could not connect:" . mysql_error());
+mysql_select_db("happy_rent", $con);
+$sql = "INSERT INTO brief_info(lng, lat, imgSrc, area, price, rooms, halls, level) VALUES($lng, $lat, '$imgSrc', $area, $price, $rooms, $halls, $level)";
+mysql_query($sql);
+
+mysql_close($con);
 ?>
