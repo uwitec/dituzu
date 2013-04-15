b1084717f95c4dbdc8f68f83d30e5a1d0c3af8e1
huster.zhangpeng <huster.zhangpeng@gmail.com>
2013-04-15 03:23:45 +0000

    完成ajax。js经测试支持各种浏览器

--- js/ajax.js
+++ js/ajax.js
@@ -20,17 +20,41 @@ function request(paramObj){
     }
 
     xmlHttp.onreadystatechange = function(){
-        if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-            if(paramobj.success instance
-            paramObj.success(xmlHttp.responseText);
+        if(xmlHttp.readyState == 4){
+            if(xmlHttp.status < 400){
+                paramObj.success(xmlHttp.responseText);
+            } else {
+                paramObj.error();
+            }
         }
     };
 
     xmlHttp.open(paramObj.method, paramObj.url, true);
     if(paramObj.method == "POST"){
         xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlHttp.send(paramObj.dataString);
+        var paramStr = "";
+        var counter = 0;
+        for(var name in paramObj.data){
+            if(counter != 0)
+                paramStr += "&";
+            paramStr += name + "=" + paramObj.data[name];
+            counter ++;
+        }
+        alert(paramStr);
+        xmlHttp.send(paramStr);
     }else{
         xmlHttp.send();
     }
 }
+
+request({
+    url:"b.txt",
+    method:"POST",
+    data:{name:"zp", lat:12.304},
+    success:function(data){
+        alert("success");
+    },
+    error:function(){
+        alert("error");
+    }
+});
